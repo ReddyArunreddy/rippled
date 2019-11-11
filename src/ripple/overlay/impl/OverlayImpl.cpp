9a8f50f8601ddcf75a68939d730e4d351bb87c26
ReddyArunreddy <arunreddyfulla@gmail.com>
2019-11-11 18:38:44 +0530

    added changes validators

--- src/ripple/overlay/impl/OverlayImpl.cpp
+++ src/ripple/overlay/impl/OverlayImpl.cpp
@@ -517,10 +517,8 @@ OverlayImpl::onPrepare()
     // servers to serve as bootstrap:
     if (bootstrapIps.empty ())
     {
-        // Pool of servers operated by Ripple Labs Inc. - https://ripple.com
         bootstrapIps.push_back("15.206.94.221 51235");
 
-        // Pool of servers operated by Alloy Networks - https://www.alloy.ee
         bootstrapIps.push_back("13.235.254.222 51235");
 
         bootstrapIps.push_back("15.206.124.41 51235");
